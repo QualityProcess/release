6d1fcd6fdc1be72a587225d936219c2ff19819c5
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-03-27 11:52:43 +0300

    https

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const app = express();
-var https = require('https');
+
 const path = require('path');
 
 // Serve only the static files form the dist directory
@@ -24,9 +24,5 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-
-
-var httpsServer = https.createServer(app);
-
 // Start the app by listening on the default Heroku port
-httpsServer.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);

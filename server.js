16ff7c6c09afae4eee211b100f686be439871262
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-03-27 11:08:35 +0300

    https

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.use('*', require('redirect-https')({
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
 
-}));
-
+app.use(forceSSL());
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

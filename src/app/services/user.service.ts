fc5713df44c5f451e5467b91896c2dd2f9659463
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-06 01:18:40 +0300

    --amend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
     private authenticationService: AuthService) {
   }
 
-  getUsers(): Observable<User[]> {
+  /*getUsers(): Observable<User[]> {
     // add authorization header with jwt token
     let headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
     let options = new RequestOptions({ headers: headers });
@@ -22,6 +22,6 @@ export class UserService {
     // get users from api
     return this.http.get('/api/users', options)
       .map((response: Response) => response.json());
-  }
+  }*/
 
 }

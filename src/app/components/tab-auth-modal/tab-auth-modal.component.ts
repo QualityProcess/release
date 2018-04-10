11985aa1b4c052b26b79d74f31adccf5b295bfbb
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-10 15:36:15 +0300

    --amend

--- src/app/components/tab-auth-modal/tab-auth-modal.component.ts
+++ src/app/components/tab-auth-modal/tab-auth-modal.component.ts
@@ -34,8 +34,48 @@ export class TabAuthModalComponent implements OnInit {
         login_hint: context.upn,
       };
       let authorizeEndpoint = "https://login.microsoftonline.com/common/oauth2/authorize?" + this.toQueryString(queryParams, null);
+
+      let hashParams = this.getHashParameters();
+      console.log(hashParams);
+      if (hashParams["error"]) {
+        // Authentication/authorization failed
+        microsoftTeams.authentication.notifyFailure(hashParams["error"]);
+      } else if (hashParams["access_token"]) {
+        // Get the stored state parameter and compare with incoming state
+        // This validates that the data is coming from Azure AD
+        let expectedState = localStorage.getItem("simple.state");
+        if (expectedState !== hashParams["state"]) {
+          // State does not match, report error
+          microsoftTeams.authentication.notifyFailure("StateDoesNotMatch");
+        } else {
+          // Success: return token information to the tab
+          microsoftTeams.authentication.notifySuccess({
+            idToken: hashParams["id_token"],
+            accessToken: hashParams["access_token"],
+            tokenType: hashParams["token_type"],
+            expiresIn: hashParams["expires_in"]
+          })
+        }
+      } else {
+        // Unexpected condition: hash does not contain error or access_token parameter
+        microsoftTeams.authentication.notifyFailure("UnexpectedFailure");
+      }
+
       window.location.assign(authorizeEndpoint);
+
+
+
     });
+  }
+
+  getHashParameters() {
+    var hash = window.location.hash.substring(1);
+    var params = {}
+    hash.split('&').map(hk => {
+      let temp = hk.split('=');
+      params[temp[0]] = temp[1]
+    });
+    return params;
   }
 
   _guid(): string {

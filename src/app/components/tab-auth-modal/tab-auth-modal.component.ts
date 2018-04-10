294462133cd21949d3fb189ddec9515cb0a28d03
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-10 15:24:06 +0300

    --amend

--- src/app/components/tab-auth-modal/tab-auth-modal.component.ts
+++ src/app/components/tab-auth-modal/tab-auth-modal.component.ts
@@ -23,8 +23,8 @@ export class TabAuthModalComponent implements OnInit {
       // Go to the Azure AD authorization endpoint
       let queryParams = {
         client_id: "ee2ec70a-88b0-4a5d-8ae2-e924d65965f9",
-        response_type: "id_token token",
-        response_mode: "fragment",
+        //response_type: "code",
+        //response_mode: "fragment",
         resource: "https://graph.microsoft.com/User.Read openid",
         redirect_uri: window.location.origin + "/tab-auth-end",
         nonce: this._guid(),

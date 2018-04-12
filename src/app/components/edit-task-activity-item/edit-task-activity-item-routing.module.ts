8ebccff0e298cb655b499327033f2ccb286a0347
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-12 17:23:30 +0300

    check qa chacked

--- src/app/components/edit-task-activity-item/edit-task-activity-item-routing.module.ts
+++ src/app/components/edit-task-activity-item/edit-task-activity-item-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 // resolvers
 import { TaskActivityItemsResolver } from './edit-task-activity-items.resolver';
+import { ProjectResolver } from './../project/project.resolver';
+import { TaskResolver } from './../task/task.resolver';
 
 // components
 import { EditTaskActivityItemComponent } from './edit-task-activity-item.component';
@@ -12,7 +14,9 @@ const routes: Routes = [
     path: '',
     component: EditTaskActivityItemComponent,
     resolve: {
-      taskActivityItemsData: TaskActivityItemsResolver
+      taskActivityItemsData: TaskActivityItemsResolver,
+      projectData: ProjectResolver,
+      taskData: TaskResolver
     }
   }
 ];

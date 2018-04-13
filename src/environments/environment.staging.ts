5d6b8f024fbca06253b026b08c3a915034bd7c17
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-13 18:42:55 +0300

    --amend

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,12 +3,12 @@ export const environment = {
   apiURL: "https://qualityprocess-staging.herokuapp.com/api/v1",
   adal5Config: {
     tenant: 'common',
-    clientId: '60531b38-e543-4327-8d7f-67dd3b8f8127',
+    clientId: 'ee2ec70a-88b0-4a5d-8ae2-e924d65965f9',
     postLogoutRedirectUri: window.location.origin + '/logout'
   },
   msTeamsConfig: {
     tenant: 'common',
-    clientId: '60531b38-e543-4327-8d7f-67dd3b8f8127',
+    clientId: 'ee2ec70a-88b0-4a5d-8ae2-e924d65965f9',
     redirectUri: window.location.origin + "/tab-auth-end",
     cacheLocation: "localStorage",
     navigateToLoginRequestUrl: false,

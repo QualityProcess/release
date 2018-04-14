69e3ab5d15eda753360d8a130cea94b43ef4cabd
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-14 10:53:05 +0300

    Azure AD authentication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
   apiURL: "https://qualityprocess.herokuapp.com/api/v1",
   adalAppId: "dad407b2-83d0-4e52-9b43-ba1940b9d9e9",
   adal5Config: {
-    tenant: 'common',
-    clientId: 'dad407b2-83d0-4e52-9b43-ba1940b9d9e9',
-    postLogoutRedirectUri: window.location.origin + '/logout'
+    tenant: "common",
+    clientId: "dad407b2-83d0-4e52-9b43-ba1940b9d9e9",
+    postLogoutRedirectUri: window.location.origin + "/logout"
   },
   msTeamsConfig: {
-    tenant: 'common',
-    clientId: 'dad407b2-83d0-4e52-9b43-ba1940b9d9e9',
+    tenant: "common",
+    clientId: "dad407b2-83d0-4e52-9b43-ba1940b9d9e9",
     redirectUri: window.location.origin + "/tab-auth-end",
     cacheLocation: "localStorage",
     navigateToLoginRequestUrl: false,

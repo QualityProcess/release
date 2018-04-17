a1020bf7e4b70251c5184747a02a460439f28e01
Chelowe4ok <Chelowe4ok@DESKTOP-J3MNB84>
2018-04-17 22:17:46 +0300

    --amend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,6 @@ export const environment = {
     redirectUri: window.location.origin + "/tab-auth-silent",
     cacheLocation: "localStorage",
     navigateToLoginRequestUrl: false,
-    prompt: "admin_consent",
     extraQueryParameters: "",
     resource: "https://graph.windows.net/"
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,6 @@ export const environment = {
     redirectUri: window.location.origin + "/tab-auth-silent",
     cacheLocation: "localStorage",
     navigateToLoginRequestUrl: false,
-    prompt: "admin_consent",
     extraQueryParameters: "",
     resource: "https://graph.windows.net/"
   },
